2469b975fb1e9e2583f4cbe6d3271dee8f781226
Gustavo Madeira Santana <gustavomsbt@gmail.com>
2015-11-21 15:08:42 -0200

    classe ShotsController com funcao lambda, generics e curring

---
+++ Assets/Scripts/ShotsController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public static class ShotsController {
+
+	public static Func<A, Func<B, R>> Curry<A, B, R>(this Func<A, B, R> f)
+	{
+		return a => b => f(a, b);
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
